5f334dea6bfa82dedf04022f772151d46bfd434b
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2025-10-24 00:21:50 +0700

    feat: add PlanService to UserCoursesModule imports for enhanced functionality

--- be/src/user_courses/user_courses.module.ts
+++ be/src/user_courses/user_courses.module.ts
@@ -4,9 +4,10 @@ import { UserCoursesController } from './user_courses.controller';
 import { UserCourse } from './entities/user_course.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
+import { PlanService } from 'src/plan/plan.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserCourse, User])],
+  imports: [TypeOrmModule.forFeature([UserCourse, User]), PlanService],
   controllers: [UserCoursesController],
   providers: [UserCoursesService],
 })
